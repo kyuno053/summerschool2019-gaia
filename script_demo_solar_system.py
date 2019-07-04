0ecd04308e425df10d7701e689473b16c36c7cce
Kevin OGIER <22393611+KevinOGIER@users.noreply.github.com>
2019-07-04 10:43:36 +0200

    Update the current script

--- script_demo_solar_system.py
+++ script_demo_solar_system.py
@@ -2,34 +2,33 @@
 # Created by Gaia Summer School team
 
 from gaia.cu9.ari.gaiaorbit.script import EventScriptingInterface
-
 gs = EventScriptingInterface.instance()
 
+
 def showAnObject( obj, msg ):
-    i=0
     gs.setHeadlineMessage(obj)
     gs.setSubheadMessage(msg)
     gs.setCameraFocus(obj)
     gs.goToObject(obj, 20.0, 4.5)
-    for i in range(120):
+    for i in range(0,90,3):
         gs.setCameraPositionAndFocus(obj, "Sol", i, 30)
-        gs.sleep(0.04)
+        gs.sleep(0.05)
     
-    gs.sleep(1)
     return;
+
 def zoomOut():
-	gs.setCameraFocus("Earth")
-	gs.goToObject("Earth",0.0000000000005,5)
-	return;
+    gs.setCameraFocus("Earth")
+    gs.goToObject("Earth",0.0000000000005,5)
+    return;
+
 # Disable input
-gs.disableInput()
-gs.cameraStop()
-gs.minimizeInterfaceWindow()
+#gs.disableInput()
+#gs.cameraStop()
+#gs.minimizeInterfaceWindow()
 # Welcome
 gs.setVisibility("element.labels", False)
 gs.setCinematicCamera(True)
-gs.setHeadlineMessage("Welcome to the Gaia Sky")
-gs.setSubheadMessage("Explore Gaia, the Solar System and the whole Galaxy!")
+gs.setHeadlineMessage("A long time ago in a galaxy far far away")
 gs.sleep(2)
 
 # Sun
@@ -39,32 +38,476 @@ gs.setCameraFocus("Sol")
 gs.goToObject("Sol", 20.0, 4.5)
 gs.sleep(2)
 
+
 #Mercury
-showAnObject( obj="Mercury", msg="This is Mercury")
+try:
+    showAnObject( obj="Mercury", msg="This is Mercury")
+except:
+    gs.setHeadlineMessage("Mercury is not working, IDKW")
 
 #Venus
-showAnObject( obj="Venus", msg="This is Venus")
+try:
+    showAnObject( obj="Venus", msg="This is Venus")
+except:
+    gs.setHeadlineMessage("Venus is not working, IDKW")
 
 # Earth
-showAnObject( obj="Earth", msg="This is our planet, the Earth")
+try:
+    showAnObject( obj="Earth", msg="This is our planet, the Earth")
+except:
+    gs.setHeadlineMessage("Earth is not working, IDKW")
 
 #Mars
-showAnObject( obj="Mars", msg="This is Mars.... everyone wants to go there")
+try:
+    showAnObject( obj="Mars", msg="This is Mars")
+except:
+    gs.setHeadlineMessage("MARS is not working, IDKW")
 
 #Jupiter
-showAnObject( obj="Jupiter", msg="This is Jupiter")
+try:
+    showAnObject( obj="Jupiter", msg="This is Jupiter")
+except:
+    gs.setHeadlineMessage("Jupiter is not working, IDKW")
 
 #Saturn
-showAnObject( obj="Saturn", msg="This is Saturn")
+try:
+    showAnObject( obj="Saturn", msg="This is Saturn")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
 
 #Uranus
-showAnObject( obj="Uranus", msg="This is Uranus")
+try:
+    showAnObject( obj="Uranus", msg="This is Uranus")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
 
 #Neptune
-showAnObject( obj="Neptune", msg="This is Neptune")
+try:
+    showAnObject( obj="Neptune", msg="This is Neptune")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
 
 #Pluto
-showAnObject( obj="Pluto", msg="This is Pluto ... and it is not a planet anymore :( ")
+try:
+    showAnObject( obj="Pluto", msg="This is Pluto ... and it is not a planet anymore :( ")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#zoom out
+zoomOut()
+# Maximize interface and enable input
+gs.setVisibility("element.labels", True)
+gs.setCinematicCamera(False)
+gs.clearAllMessages()
+gs.maximizeInterfaceWindow()
+gs.enableInput()
+
+# This script is executed when the GaiaSandbox is first started.
+# Created by Gaia Summer School team
+
+from gaia.cu9.ari.gaiaorbit.script import EventScriptingInterface
+gs = EventScriptingInterface.instance()
+
+
+def showAnObject( obj, msg ):
+    gs.setHeadlineMessage(obj)
+    gs.setSubheadMessage(msg)
+    gs.setCameraFocus(obj)
+    gs.goToObject(obj, 20.0, 4.5)
+    for i in range(0,90,3):
+        gs.setCameraPositionAndFocus(obj, "Sol", i, 30)
+        gs.sleep(0.05)
+    
+    return;
+
+def zoomOut():
+    gs.setCameraFocus("Earth")
+    gs.goToObject("Earth",0.0000000000005,5)
+    return;
+
+# Disable input
+#gs.disableInput()
+#gs.cameraStop()
+#gs.minimizeInterfaceWindow()
+# Welcome
+gs.setVisibility("element.labels", False)
+gs.setCinematicCamera(True)
+gs.setHeadlineMessage("A long time ago in a galaxy far far away")
+gs.sleep(2)
+
+# Sun
+gs.setHeadlineMessage("Sun")
+gs.setSubheadMessage("This is our star, the Sun")
+gs.setCameraFocus("Sol")
+gs.goToObject("Sol", 20.0, 4.5)
+gs.sleep(2)
+
+
+#Mercury
+try:
+    showAnObject( obj="Mercury", msg="This is Mercury")
+except:
+    gs.setHeadlineMessage("Mercury is not working, IDKW")
+
+#Venus
+try:
+    showAnObject( obj="Venus", msg="This is Venus")
+except:
+    gs.setHeadlineMessage("Venus is not working, IDKW")
+
+# Earth
+try:
+    showAnObject( obj="Earth", msg="This is our planet, the Earth")
+except:
+    gs.setHeadlineMessage("Earth is not working, IDKW")
+
+#Mars
+try:
+    showAnObject( obj="Mars", msg="This is Mars")
+except:
+    gs.setHeadlineMessage("MARS is not working, IDKW")
+
+#Jupiter
+try:
+    showAnObject( obj="Jupiter", msg="This is Jupiter")
+except:
+    gs.setHeadlineMessage("Jupiter is not working, IDKW")
+
+#Saturn
+try:
+    showAnObject( obj="Saturn", msg="This is Saturn")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Uranus
+try:
+    showAnObject( obj="Uranus", msg="This is Uranus")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Neptune
+try:
+    showAnObject( obj="Neptune", msg="This is Neptune")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Pluto
+try:
+    showAnObject( obj="Pluto", msg="This is Pluto ... and it is not a planet anymore :( ")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#zoom out
+zoomOut()
+# Maximize interface and enable input
+gs.setVisibility("element.labels", True)
+gs.setCinematicCamera(False)
+gs.clearAllMessages()
+gs.maximizeInterfaceWindow()
+gs.enableInput()
+
+# This script is executed when the GaiaSandbox is first started.
+# Created by Gaia Summer School team
+
+from gaia.cu9.ari.gaiaorbit.script import EventScriptingInterface
+gs = EventScriptingInterface.instance()
+
+
+def showAnObject( obj, msg ):
+    gs.setHeadlineMessage(obj)
+    gs.setSubheadMessage(msg)
+    gs.setCameraFocus(obj)
+    gs.goToObject(obj, 20.0, 4.5)
+    for i in range(0,90,3):
+        gs.setCameraPositionAndFocus(obj, "Sol", i, 30)
+        gs.sleep(0.05)
+    
+    return;
+
+def zoomOut():
+    gs.setCameraFocus("Earth")
+    gs.goToObject("Earth",0.0000000000005,5)
+    return;
+
+# Disable input
+#gs.disableInput()
+#gs.cameraStop()
+#gs.minimizeInterfaceWindow()
+# Welcome
+gs.setVisibility("element.labels", False)
+gs.setCinematicCamera(True)
+gs.setHeadlineMessage("A long time ago in a galaxy far far away")
+gs.sleep(2)
+
+# Sun
+gs.setHeadlineMessage("Sun")
+gs.setSubheadMessage("This is our star, the Sun")
+gs.setCameraFocus("Sol")
+gs.goToObject("Sol", 20.0, 4.5)
+gs.sleep(2)
+
+
+#Mercury
+try:
+    showAnObject( obj="Mercury", msg="This is Mercury")
+except:
+    gs.setHeadlineMessage("Mercury is not working, IDKW")
+
+#Venus
+try:
+    showAnObject( obj="Venus", msg="This is Venus")
+except:
+    gs.setHeadlineMessage("Venus is not working, IDKW")
+
+# Earth
+try:
+    showAnObject( obj="Earth", msg="This is our planet, the Earth")
+except:
+    gs.setHeadlineMessage("Earth is not working, IDKW")
+
+#Mars
+try:
+    showAnObject( obj="Mars", msg="This is Mars")
+except:
+    gs.setHeadlineMessage("MARS is not working, IDKW")
+
+#Jupiter
+try:
+    showAnObject( obj="Jupiter", msg="This is Jupiter")
+except:
+    gs.setHeadlineMessage("Jupiter is not working, IDKW")
+
+#Saturn
+try:
+    showAnObject( obj="Saturn", msg="This is Saturn")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Uranus
+try:
+    showAnObject( obj="Uranus", msg="This is Uranus")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Neptune
+try:
+    showAnObject( obj="Neptune", msg="This is Neptune")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Pluto
+try:
+    showAnObject( obj="Pluto", msg="This is Pluto ... and it is not a planet anymore :( ")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#zoom out
+zoomOut()
+# Maximize interface and enable input
+gs.setVisibility("element.labels", True)
+gs.setCinematicCamera(False)
+gs.clearAllMessages()
+gs.maximizeInterfaceWindow()
+gs.enableInput()
+
+# This script is executed when the GaiaSandbox is first started.
+# Created by Gaia Summer School team
+
+from gaia.cu9.ari.gaiaorbit.script import EventScriptingInterface
+gs = EventScriptingInterface.instance()
+
+
+def showAnObject( obj, msg ):
+    gs.setHeadlineMessage(obj)
+    gs.setSubheadMessage(msg)
+    gs.setCameraFocus(obj)
+    gs.goToObject(obj, 20.0, 4.5)
+    for i in range(0,90,3):
+        gs.setCameraPositionAndFocus(obj, "Sol", i, 30)
+        gs.sleep(0.05)
+    
+    return;
+
+def zoomOut():
+    gs.setCameraFocus("Earth")
+    gs.goToObject("Earth",0.0000000000005,5)
+    return;
+
+# Disable input
+#gs.disableInput()
+#gs.cameraStop()
+#gs.minimizeInterfaceWindow()
+# Welcome
+gs.setVisibility("element.labels", False)
+gs.setCinematicCamera(True)
+gs.setHeadlineMessage("A long time ago in a galaxy far far away")
+gs.sleep(2)
+
+# Sun
+gs.setHeadlineMessage("Sun")
+gs.setSubheadMessage("This is our star, the Sun")
+gs.setCameraFocus("Sol")
+gs.goToObject("Sol", 20.0, 4.5)
+gs.sleep(2)
+
+
+#Mercury
+try:
+    showAnObject( obj="Mercury", msg="This is Mercury")
+except:
+    gs.setHeadlineMessage("Mercury is not working, IDKW")
+
+#Venus
+try:
+    showAnObject( obj="Venus", msg="This is Venus")
+except:
+    gs.setHeadlineMessage("Venus is not working, IDKW")
+
+# Earth
+try:
+    showAnObject( obj="Earth", msg="This is our planet, the Earth")
+except:
+    gs.setHeadlineMessage("Earth is not working, IDKW")
+
+#Mars
+try:
+    showAnObject( obj="Mars", msg="This is Mars")
+except:
+    gs.setHeadlineMessage("MARS is not working, IDKW")
+
+#Jupiter
+try:
+    showAnObject( obj="Jupiter", msg="This is Jupiter")
+except:
+    gs.setHeadlineMessage("Jupiter is not working, IDKW")
+
+#Saturn
+try:
+    showAnObject( obj="Saturn", msg="This is Saturn")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Uranus
+try:
+    showAnObject( obj="Uranus", msg="This is Uranus")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Neptune
+try:
+    showAnObject( obj="Neptune", msg="This is Neptune")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Pluto
+try:
+    showAnObject( obj="Pluto", msg="This is Pluto ... and it is not a planet anymore :( ")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#zoom out
+zoomOut()
+# Maximize interface and enable input
+gs.setVisibility("element.labels", True)
+gs.setCinematicCamera(False)
+gs.clearAllMessages()
+gs.maximizeInterfaceWindow()
+gs.enableInput()
+
+# This script is executed when the GaiaSandbox is first started.
+# Created by Gaia Summer School team
+
+from gaia.cu9.ari.gaiaorbit.script import EventScriptingInterface
+gs = EventScriptingInterface.instance()
+
+
+def showAnObject( obj, msg ):
+    gs.setHeadlineMessage(obj)
+    gs.setSubheadMessage(msg)
+    gs.setCameraFocus(obj)
+    gs.goToObject(obj, 20.0, 4.5)
+    for i in range(0,90,3):
+        gs.setCameraPositionAndFocus(obj, "Sol", i, 30)
+        gs.sleep(0.05)
+    
+    return;
+
+def zoomOut():
+    gs.setCameraFocus("Earth")
+    gs.goToObject("Earth",0.0000000000005,5)
+    return;
+
+# Disable input
+#gs.disableInput()
+#gs.cameraStop()
+#gs.minimizeInterfaceWindow()
+# Welcome
+gs.setVisibility("element.labels", False)
+gs.setCinematicCamera(True)
+gs.setHeadlineMessage("A long time ago in a galaxy far far away")
+gs.sleep(2)
+
+# Sun
+gs.setHeadlineMessage("Sun")
+gs.setSubheadMessage("This is our star, the Sun")
+gs.setCameraFocus("Sol")
+gs.goToObject("Sol", 20.0, 4.5)
+gs.sleep(2)
+
+
+#Mercury
+try:
+    showAnObject( obj="Mercury", msg="This is Mercury")
+except:
+    gs.setHeadlineMessage("Mercury is not working, IDKW")
+
+#Venus
+try:
+    showAnObject( obj="Venus", msg="This is Venus")
+except:
+    gs.setHeadlineMessage("Venus is not working, IDKW")
+
+# Earth
+try:
+    showAnObject( obj="Earth", msg="This is our planet, the Earth")
+except:
+    gs.setHeadlineMessage("Earth is not working, IDKW")
+
+#Mars
+try:
+    showAnObject( obj="Mars", msg="This is Mars")
+except:
+    gs.setHeadlineMessage("MARS is not working, IDKW")
+
+#Jupiter
+try:
+    showAnObject( obj="Jupiter", msg="This is Jupiter")
+except:
+    gs.setHeadlineMessage("Jupiter is not working, IDKW")
+
+#Saturn
+try:
+    showAnObject( obj="Saturn", msg="This is Saturn")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Uranus
+try:
+    showAnObject( obj="Uranus", msg="This is Uranus")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Neptune
+try:
+    showAnObject( obj="Neptune", msg="This is Neptune")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
+
+#Pluto
+try:
+    showAnObject( obj="Pluto", msg="This is Pluto ... and it is not a planet anymore :( ")
+except:
+    gs.setHeadlineMessage("Saturn is not working, IDKW")
 
 #zoom out
 zoomOut()
